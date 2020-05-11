ce306b3ef162fe60e5495515993a2c1f1e00ce24
Luke Chen <luke.chen.2010@gmail.com>
2020-05-11 15:52:48 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -11,13 +11,16 @@ class Artist
     @songs = []
   end
 
-  def self.find_by_name(name)
-    @@artists.detect {|a| a.name == name}
-  end
+  #def self.find_by_name(name)
+   # @@artists.detect {|a| a.name == name}
+  #end
 
   def self.all
     @@artists
   end
+  
+  extend Findable
+
 
   extend Memorable
 
